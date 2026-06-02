2c2f2acee90acde5404745a471e6b0032acb8a3a
Brandon Rodriguez <brandon.rod182@gmail.com>
2026-06-01 21:14:58 -0700

    fix: adjust background styles in ParallaxStarfield component for improved visual consistency

--- src/components/parallax-starfield.tsx
+++ src/components/parallax-starfield.tsx
@@ -17,14 +17,16 @@ export function ParallaxStarfield() {
     >
       <div
         ref={backgroundRef}
-        className="absolute inset-0 brightness-[1.35] contrast-[1.05] will-change-[background-position] bg-repeat"
+        className="absolute inset-0 brightness-[0.8] contrast-[0.92] saturate-[0.85] will-change-[background-position] bg-repeat"
         style={{
           backgroundImage: `url('${HERO_BG}')`,
           backgroundSize: `${TILE_SIZE_PX}px ${TILE_SIZE_PX}px`,
         }}
       />
       <div className="absolute inset-0 bg-[radial-gradient(ellipse_90%_70%_at_50%_0%,transparent_0%,hsl(0_0%_7%_/0.18)_55%,hsl(0_0%_7%_/0.55)_100%)]" />
-      <div className="absolute inset-0 bg-canvas/15" />
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_75%_65%_at_50%_45%,transparent_0%,hsl(0_0%_5%/0.45)_70%,hsl(0_0%_4%/0.8)_100%)]" />
+      <div className="absolute inset-0 bg-canvas/35" />
+      <div className="absolute inset-0 bg-[radial-gradient(ellipse_60%_50%_at_50%_30%,hsl(205_100%_66%/0.06),transparent_60%)]" />
     </div>
   )
 }
